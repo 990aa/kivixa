d9edd543969a254733919446823a463e90666ffa
990aa <099ahadabdul@gmail.com>
2025-09-11 23:20:01 +0530

    feature set 4 additional

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
+import 'services/python_client.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,70 +57,56 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int _counter = 0;
+  String? _ocrResult;
+  bool _loading = false;
+
+  Future<void> _runOcr() async {
+    setState(() { _loading = true; _ocrResult = null; });
+    final picked = await FilePicker.platform.pickFiles(type: FileType.image);
+    if (picked != null && picked.files.single.path != null) {
+      final file = File(picked.files.single.path!);
+      final client = await PythonServiceClient.detect();
+      if (client != null) {
+        final req = http.MultipartRequest('POST', Uri.parse('${client.baseUrl}/ocr'));
+        req.files.add(await http.MultipartFile.fromPath('file', file.path));
+        final streamed = await req.send();
+        final resp = await streamed.stream.bytesToString();
+        setState(() { _ocrResult = resp; });
+      } else {
+        setState(() { _ocrResult = 'Python service not available'; });
+      }
+    }
+    setState(() { _loading = false; });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: _loading ? null : _runOcr,
+              child: const Text('Run OCR via Python Service'),
+            ),
+            if (_loading) const Padding(
+              padding: EdgeInsets.all(16),
+              child: CircularProgressIndicator(),
+            ),
+            if (_ocrResult != null) Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(_ocrResult!),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
