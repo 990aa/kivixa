34cb07f2c6e52f3f6a6b4e6c155f9655c434c42c
990aa <099ahadabdul@gmail.com>
2025-09-13 18:49:21 +0530

    providers update

---
+++ lib/providers.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/theme/theme_service.dart';
+
+final sharedPreferencesProvider = FutureProvider<SharedPreferences>((ref) async {
+  return await SharedPreferences.getInstance();
+});
+
+final themeProvider =
+    StateNotifierProvider<ThemeModeNotifier, ThemeModeSetting>((ref) {
+  final prefs = ref.watch(sharedPreferencesProvider).asData!.value;
+  return ThemeModeNotifier(prefs);
+});
