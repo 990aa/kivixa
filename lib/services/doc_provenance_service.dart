c696ac9c500729324a014e64681eebd3a19d738c
990aa <099ahadabdul@gmail.com>
2025-09-13 16:37:17 +0530

    doc provence

---
+++ lib/services/doc_provenance_service.dart
@@ -0,0 +1,31 @@
+import 'package:kivixa/data/database.dart';
+import 'package:drift/drift.dart';
+
+enum ProvenanceType {
+  merge,
+  split,
+  raster,
+}
+
+class DocProvenanceService {
+  final AppDatabase _db;
+
+  DocProvenanceService(this._db);
+
+  Future<void> recordProvenance({
+    required String newDocumentId,
+    required ProvenanceType type,
+    required List<String> sourceDocumentIds,
+  }) async {
+    final companion = DocProvenanceCompanion.insert(
+      newDocumentId: newDocumentId,
+      type: type.toString(),
+      sourceDocumentIds: sourceDocumentIds.join(','),
+    );
+    await _db.into(_db.docProvenance).insert(companion);
+  }
+
+  Future<List<DocProvenanceData>> getProvenance(String documentId) async {
+    return (_db.select(_db.docProvenance)..where((tbl) => tbl.newDocumentId.equals(documentId))).get();
+  }
+}
