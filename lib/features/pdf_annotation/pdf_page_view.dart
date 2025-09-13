87345604cd9c6523bbfb798400c7bccc3ba516de
990aa <099ahadabdul@gmail.com>
2025-09-13 18:15:45 +0530

    pdf annotation

---
+++ lib/features/pdf_annotation/pdf_page_view.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path/path.dart' as p;
+import 'package:flutter/physics.dart';
+
+class PdfPageView extends StatefulWidget {
+  final String pdfPath;
+
+  const PdfPageView({
+    super.key,
+    required this.pdfPath,
+  });
+
+  @override
+  State<PdfPageView> createState() => _PdfPageViewState();
+}
+
+class _PdfPageViewState extends State<PdfPageView> {
+  late PageController _pageController;
+  List<pw.Widget> _pages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _loadPdf();
+  }
+
+  Future<void> _loadPdf() async {
+    final file = File(widget.pdfPath);
+    final document = pw.Document.load(file);
+    final pageCount = document.document.pages.length;
+    final pages = <pw.Widget>[];
+    for (var i = 0; i < pageCount; i++) {
+      // This is a simplified page rendering. A more robust solution would
+      // involve a more sophisticated PDF rendering library.
+      pages.add(pw.Container(
+        child: pw.Text('Page ${i + 1}'),
+      ));
+    }
+    setState(() {
+      _pages = pages;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      controller: _pageController,
+      itemCount: _pages.length,
+      itemBuilder: (context, index) {
+        return _pages[index] as Widget;
+      },
+      physics: const BouncingScrollPhysics(),
+    );
+  }
+}
