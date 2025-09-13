2ae2a76ac194df2eddb38edda2466463aa7a0b3c
990aa <099ahadabdul@gmail.com>
2025-09-13 18:53:50 +0530

    pkg riverpool

--- lib/providers.dart
+++ lib/providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'core/theme/theme_service.dart';
 import 'data/database.dart';
+import 'data/repository.dart';
 
 final sharedPreferencesProvider = FutureProvider<SharedPreferences>((ref) async {
   return await SharedPreferences.getInstance();
@@ -15,4 +16,14 @@ final themeProvider =
 
 final appDatabaseProvider = Provider<AppDatabase>((ref) {
   return AppDatabase();
+});
+
+final documentRepositoryProvider = Provider<DocumentRepository>((ref) {
+  final db = ref.watch(appDatabaseProvider);
+  return DocumentRepository(db);
+});
+
+final documentsProvider = StreamProvider<List<DocumentData>>((ref) {
+  final repository = ref.watch(documentRepositoryProvider);
+  return repository.watchDocuments();
 });
