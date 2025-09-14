708091f43f01c285d211d30c1e9411f57a692bc9
990aa <099ahadabdul@gmail.com>
2025-09-14 11:34:12 +0530

    errors fix 31

--- lib/data/migrations/002_add_folder_support.dart
+++
@@ -1,5 +0,0 @@
-
-final String migration002 = '''
-ALTER TABLE documents ADD COLUMN parent_id INTEGER REFERENCES documents(id) ON DELETE SET NULL;
-ALTER TABLE documents ADD COLUMN type TEXT NOT NULL DEFAULT 'document';
-''';
