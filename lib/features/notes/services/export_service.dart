152d2ebb56497d826cffc44c3b633c0c764a0b5a
990aa <099ahadabdul@gmail.com>
2025-09-19 22:56:36 +0530

    export

--- lib/features/notes/services/export_service.dart
+++ lib/features/notes/services/export_service.dart
@@ -1,6 +1,7 @@
 
 import 'dart:convert';
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
@@ -52,6 +53,15 @@ class ExportService {
     final path = '${directory.path}/${document.title}.pdf';
     final file = File(path);
     await file.writeAsBytes(await pdf.save());
+    return path;
+  }
+
+  Future<String> exportToPng(
+      NoteDocument document, Uint8List pngBytes, int pageNumber) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${document.title}_page_$pageNumber.png';
+    final file = File(path);
+    await file.writeAsBytes(pngBytes);
     return path;
   }
 }
