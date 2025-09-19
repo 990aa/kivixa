b628a68f4335ff8292fce5c1e8c69b8c32bf8819
990aa <099ahadabdul@gmail.com>
2025-09-19 23:35:18 +0530

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kivixa/features/notes/screens/folder_management_screen.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kivixa',
-      theme: ThemeData.dark(),
-      home: const FolderManagementScreen(),
+    return ShadcnTheme(
+      data: ShadcnThemeData(
+        brightness: Brightness.dark,
+        primary: ShadcnColor.fromRgb(0, 122, 255),
+      ),
+      child: MaterialApp(
+        title: 'Kivixa',
+        theme: ThemeData.dark(),
+        home: const FolderManagementScreen(),
+      ),
     );
   }
 }
