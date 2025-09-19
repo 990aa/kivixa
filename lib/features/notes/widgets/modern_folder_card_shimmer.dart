44d7af6a4b239aa42cda8aa3c52fb2c09355a627
990aa <099ahadabdul@gmail.com>
2025-09-19 23:50:37 +0530

    glassmorphic folder cards with micro interactions

---
+++ lib/features/notes/widgets/modern_folder_card_shimmer.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+import 'package:shimmer/shimmer.dart';
+
+class ModernFolderCardShimmer extends StatelessWidget {
+  const ModernFolderCardShimmer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final shimmerBaseColor = Colors.grey.shade700;
+    final shimmerHighlightColor = Colors.grey.shade600;
+
+    return Shimmer.fromColors(
+      baseColor: shimmerBaseColor,
+      highlightColor: shimmerHighlightColor,
+      child: GlassmorphicContainer(
+        width: double.infinity,
+        height: 180,
+        borderRadius: 16,
+        blur: 10,
+        border: 1,
+        linearGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.grey.withOpacity(0.2),
+            Colors.grey.withOpacity(0.1),
+          ],
+          stops: const [0.1, 1],
+        ),
+        borderGradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Colors.white.withOpacity(0.5),
+            Colors.white.withOpacity(0.2),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: shimmerBaseColor,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: shimmerBaseColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: theme.textTheme.titleLarge?.fontSize,
+                    decoration: BoxDecoration(
+                      color: shimmerBaseColor,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Container(
+                    width: 120,
+                    height: theme.textTheme.bodySmall?.fontSize,
+                    decoration: BoxDecoration(
+                      color: shimmerBaseColor,
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
