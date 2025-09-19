e4e00397c058d9d0a3b7d24692cec1f0edda42c9
990aa <099ahadabdul@gmail.com>
2025-09-19 12:03:17 +0530

    db helper

--- lib/helpers/database_helper.dart
+++ lib/helpers/database_helper.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import '../models/folder.dart';
+import '../models/pdf.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _instance = DatabaseHelper._internal();
@@ -19,7 +20,7 @@ class DatabaseHelper {
 
   Future<Database> _initDatabase() async {
     String path = join(await getDatabasesPath(), 'kivixa.db');
-    return await openDatabase(path, version: 1, onCreate: _onCreate);
+    return await openDatabase(path, version: 2, onCreate: _onCreate, onUpgrade: _onUpgrade);
   }
 
   Future _onCreate(Database db, int version) async {
@@ -29,9 +30,32 @@ class DatabaseHelper {
         name TEXT,
         cover TEXT,
         createdAt TEXT,
-        colorValue INTEGER
+        colorValue INTEGER,
+        parentId INTEGER
       )
     ''');
+    await db.execute('''
+      CREATE TABLE pdfs(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        path TEXT,
+        folderId INTEGER
+      )
+    ''');
+  }
+
+  Future _onUpgrade(Database db, int oldVersion, int newVersion) async {
+    if (oldVersion < 2) {
+      await db.execute('ALTER TABLE folders ADD COLUMN parentId INTEGER');
+      await db.execute('''
+        CREATE TABLE pdfs(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          name TEXT,
+          path TEXT,
+          folderId INTEGER
+        )
+      ''');
+    }
   }
 
   Future<int> insertFolder(Folder folder) async {
@@ -39,9 +63,9 @@ class DatabaseHelper {
     return await db.insert('folders', folder.toMap());
   }
 
-  Future<List<Folder>> getFolders() async {
+  Future<List<Folder>> getFolders(int? parentId) async {
     Database db = await database;
-    var folders = await db.query('folders');
+    var folders = await db.query('folders', where: 'parentId = ?', whereArgs: [parentId]);
     List<Folder> folderList = folders.isNotEmpty
         ? folders.map((c) => Folder.fromMap(c)).toList()
         : [];
@@ -61,5 +85,34 @@ class DatabaseHelper {
   Future<int> deleteFolder(int id) async {
     Database db = await database;
     return await db.delete('folders', where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> insertPdf(Pdf pdf) async {
+    Database db = await database;
+    return await db.insert('pdfs', pdf.toMap());
+  }
+
+  Future<List<Pdf>> getPdfs(int folderId) async {
+    Database db = await database;
+    var pdfs = await db.query('pdfs', where: 'folderId = ?', whereArgs: [folderId]);
+    List<Pdf> pdfList = pdfs.isNotEmpty
+        ? pdfs.map((c) => Pdf.fromMap(c)).toList()
+        : [];
+    return pdfList;
+  }
+
+  Future<int> updatePdf(Pdf pdf) async {
+    Database db = await database;
+    return await db.update(
+      'pdfs',
+      pdf.toMap(),
+      where: 'id = ?',
+      whereArgs: [pdf.id],
+    );
+  }
+
+  Future<int> deletePdf(int id) async {
+    Database db = await database;
+    return await db.delete('pdfs', where: 'id = ?', whereArgs: [id]);
   }
 }
