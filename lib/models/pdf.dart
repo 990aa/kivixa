4f558d9e9eb4667679c906d199d17f79c6ffd742
990aa <099ahadabdul@gmail.com>
2025-09-19 12:02:24 +0530

    pdf model

---
+++ lib/models/pdf.dart
@@ -0,0 +1,26 @@
+class Pdf {
+  int? id;
+  String name;
+  String path;
+  int folderId;
+
+  Pdf({this.id, required this.name, required this.path, required this.folderId});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'path': path,
+      'folderId': folderId,
+    };
+  }
+
+  factory Pdf.fromMap(Map<String, dynamic> map) {
+    return Pdf(
+      id: map['id'],
+      name: map['name'],
+      path: map['path'],
+      folderId: map['folderId'],
+    );
+  }
+}
