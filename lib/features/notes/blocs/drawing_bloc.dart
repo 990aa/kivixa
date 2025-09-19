ff3c34eef554a4e948404889c917c63a170e5089
990aa <099ahadabdul@gmail.com>
2025-09-19 22:14:01 +0530

    state management

--- lib/features/notes/blocs/drawing_bloc.dart
+++ lib/features/notes/blocs/drawing_bloc.dart
@@ -1,13 +1,40 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/models/drawing_stroke.dart';
+import 'package:scribble/scribble.dart';
 
-abstract class DrawingEvent {}
-
-abstract class DrawingState {}
+part 'drawing_event.dart';
+part 'drawing_state.dart';
 
 class DrawingBloc extends Bloc<DrawingEvent, DrawingState> {
   DrawingBloc() : super(DrawingInitial()) {
-    // TODO: implement event handlers
+    on<DrawingStarted>((event, emit) {
+      final notifier = ScribbleNotifier();
+      emit(DrawingState(notifier));
+    });
+
+    on<ToolChanged>((event, emit) {
+      if (state is DrawingState) {
+        final notifier = (state as DrawingState).notifier;
+        notifier.setTool(event.tool);
+        emit(DrawingState(notifier));
+      }
+    });
+
+    on<ColorChanged>((event, emit) {
+      if (state is DrawingState) {
+        final notifier = (state as DrawingState).notifier;
+        notifier.setColor(event.color);
+        emit(DrawingState(notifier));
+      }
+    });
+
+    on<StrokeWidthChanged>((event, emit) {
+      if (state is DrawingState) {
+        final notifier = (state as DrawingState).notifier;
+        notifier.setStrokeWidth(event.width);
+        emit(DrawingState(notifier));
+      }
+    });
   }
 }
-
-class DrawingInitial extends DrawingState {}
