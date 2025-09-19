06d8f429137f99ac3ec57835dee6984a347b4582
990aa <099ahadabdul@gmail.com>
2025-09-19 06:39:24 +0530

    folder functions

---
+++ lib/helpers/database_helper.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/folder.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+
+  static Database? _database;
+
+  DatabaseHelper._internal();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'kivixa.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _onCreate,
+    );
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE folders(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        cover TEXT,
+        createdAt TEXT
+      )
+    ''');
+  }
+
+  Future<int> insertFolder(Folder folder) async {
+    Database db = await database;
+    return await db.insert('folders', folder.toMap());
+  }
+
+  Future<List<Folder>> getFolders() async {
+    Database db = await database;
+    var folders = await db.query('folders');
+    List<Folder> folderList = folders.isNotEmpty
+        ? folders.map((c) => Folder(
+              id: c['id'] as int,
+              name: c['name'] as String,
+              cover: c['cover'] as String,
+              createdAt: DateTime.parse(c['createdAt'] as String),
+            )).toList()
+        : [];
+    return folderList;
+  }
+
+  Future<int> updateFolder(Folder folder) async {
+    Database db = await database;
+    return await db.update('folders', folder.toMap(),
+        where: 'id = ?', whereArgs: [folder.id]);
+  }
+
+  Future<int> deleteFolder(int id) async {
+    Database db = await database;
+    return await db.delete('folders', where: 'id = ?', whereArgs: [id]);
+  }
+}
