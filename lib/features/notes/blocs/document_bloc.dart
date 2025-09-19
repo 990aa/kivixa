e7e4265f46bc4f2ef023aa9ba1c3d4268b2de921
990aa <099ahadabdul@gmail.com>
2025-09-19 22:13:30 +0530

    state management

--- lib/features/notes/blocs/document_bloc.dart
+++ lib/features/notes/blocs/document_bloc.dart
@@ -1,13 +1,50 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/services/notes_database_service.dart';
+import 'package:meta/meta.dart';
 
-abstract class DocumentEvent {}
+import '../models/note_document.dart';
 
-abstract class DocumentState {}
+part 'document_event.dart';
+part 'document_state.dart';
 
 class DocumentBloc extends Bloc<DocumentEvent, DocumentState> {
-  DocumentBloc() : super(DocumentInitial()) {
-    // TODO: implement event handlers
+  final NotesDatabaseService _notesDatabaseService;
+  Timer? _saveTimer;
+
+  DocumentBloc(this._notesDatabaseService) : super(DocumentInitial()) {
+    on<DocumentLoaded>((event, emit) async {
+      emit(DocumentLoadInProgress());
+      try {
+        final document = await _notesDatabaseService.getNoteById(event.id);
+        emit(DocumentLoadSuccess(document));
+      } catch (e) {
+        emit(DocumentLoadFailure(e.toString()));
+      }
+    });
+
+    on<DocumentSaved>((event, emit) async {
+      emit(DocumentSaveInProgress());
+      try {
+        await _notesDatabaseService.updateNote(event.document);
+        emit(DocumentSaveSuccess());
+      } catch (e) {
+        emit(DocumentSaveFailure(e.toString()));
+      }
+    });
+
+    on<DocumentContentChanged>((event, emit) {
+      _saveTimer?.cancel();
+      _saveTimer = Timer(const Duration(seconds: 30), () {
+        add(DocumentSaved(event.document));
+      });
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _saveTimer?.cancel();
+    return super.close();
   }
 }
-
-class DocumentInitial extends DocumentState {}
