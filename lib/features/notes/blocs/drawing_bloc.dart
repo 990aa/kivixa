9e5b68bf23fc98154107e100451734771ecb6ad9
990aa <099ahadabdul@gmail.com>
2025-09-19 22:38:04 +0530

    error fix

--- lib/features/notes/blocs/drawing_bloc.dart
+++ lib/features/notes/blocs/drawing_bloc.dart
@@ -3,32 +3,69 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kivixa/features/notes/models/drawing_stroke.dart';
 import 'package:scribble/scribble.dart';
 
-part 'drawing_event.dart';
-part 'drawing_state.dart';
+@immutable
+abstract class DrawingEvent {}
+
+class DrawingStarted extends DrawingEvent {}
+
+class ToolChanged extends DrawingEvent {
+  final ScribbleTool tool;
+
+  ToolChanged(this.tool);
+}
+
+class ColorChanged extends DrawingEvent {
+  final Color color;
+
+  ColorChanged(this.color);
+}
+
+class StrokeWidthChanged extends DrawingEvent {
+  final double width;
+
+  StrokeWidthChanged(this.width);
+}
+
+@immutable
+abstract class DrawingState {}
+
+class DrawingInitial extends DrawingState {}
+
+class DrawingLoadSuccess extends DrawingState {
+  final ScribbleNotifier notifier;
+
+  DrawingLoadSuccess(this.notifier);
+}
 
 class DrawingBloc extends Bloc<DrawingEvent, DrawingState> {
   DrawingBloc() : super(DrawingInitial()) {
     on<DrawingStarted>((event, emit) {
       final notifier = ScribbleNotifier();
-      emit(DrawingState(notifier));
+      emit(DrawingLoadSuccess(notifier));
     });
 
     on<ToolChanged>((event, emit) {
-      final notifier = (state as DrawingState).notifier;
-      notifier.setTool(event.tool);
-      emit(DrawingState(notifier));
-        });
+      if (state is DrawingLoadSuccess) {
+        final notifier = (state as DrawingLoadSuccess).notifier;
+        notifier.setTool(event.tool);
+        emit(DrawingLoadSuccess(notifier));
+      }
+    });
 
     on<ColorChanged>((event, emit) {
-      final notifier = (state as DrawingState).notifier;
-      notifier.setColor(event.color);
-      emit(DrawingState(notifier));
-        });
+      if (state is DrawingLoadSuccess) {
+        final notifier = (state as DrawingLoadSuccess).notifier;
+        notifier.setColor(event.color);
+        emit(DrawingLoadSuccess(notifier));
+      }
+    });
 
     on<StrokeWidthChanged>((event, emit) {
-      final notifier = (state as DrawingState).notifier;
-      notifier.setStrokeWidth(event.width);
-      emit(DrawingState(notifier));
-        });
+      if (state is DrawingLoadSuccess) {
+        final notifier = (state as DrawingLoadSuccess).notifier;
+        notifier.setStrokeWidth(event.width);
+        emit(DrawingLoadSuccess(notifier));
+      }
+    });
   }
 }
