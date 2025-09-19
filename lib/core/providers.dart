e964694b94f97bd7174b4493f66bfed00c8ffb9f
990aa <099ahadabdul@gmail.com>
2025-09-19 06:26:55 +0530

    rm

--- lib/core/providers.dart
+++
@@ -1,8 +0,0 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final connectivityProvider = StreamProvider<ConnectivityResult>((ref) {
-  return Connectivity().onConnectivityChanged.map(
-    (results) => results.isNotEmpty ? results.first : ConnectivityResult.none,
-  );
-});
--- lib/core/theme/icons.dart
+++
@@ -1,3 +0,0 @@
-library;
-
-export 'package:phosphor_flutter/phosphor_flutter.dart';
