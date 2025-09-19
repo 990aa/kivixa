0ae8cbdbf0d04eb2e5a87ba3108116dc22155c22
990aa <099ahadabdul@gmail.com>
2025-09-19 22:12:52 +0530

    state management

--- lib/features/notes/blocs/notes_bloc.dart
+++ lib/features/notes/blocs/notes_bloc.dart
@@ -1,13 +1,24 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/services/notes_database_service.dart';
+import 'package:meta/meta.dart';
 
-abstract class NotesEvent {}
+import '../models/note_document.dart';
 
-abstract class NotesState {}
+part 'notes_event.dart';
+part 'notes_state.dart';
 
 class NotesBloc extends Bloc<NotesEvent, NotesState> {
-  NotesBloc() : super(NotesInitial()) {
-    // TODO: implement event handlers
+  final NotesDatabaseService _notesDatabaseService;
+
+  NotesBloc(this._notesDatabaseService) : super(NotesInitial()) {
+    on<NotesLoaded>((event, emit) async {
+      emit(NotesLoadInProgress());
+      try {
+        final notes = await _notesDatabaseService.getAllNotes();
+        emit(NotesLoadSuccess(notes));
+      } catch (e) {
+        emit(NotesLoadFailure(e.toString()));
+      }
+    });
   }
 }
-
-class NotesInitial extends NotesState {}
