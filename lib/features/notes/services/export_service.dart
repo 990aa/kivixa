5147ca048d7787600e318d58adc581d1bbd7b627
990aa <099ahadabdul@gmail.com>
2025-09-19 22:54:11 +0530

    export

--- lib/features/notes/services/export_service.dart
+++ lib/features/notes/services/export_service.dart
@@ -2,6 +2,8 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
 import '../models/note_document.dart';
 
 class ExportService {
@@ -11,6 +13,45 @@ class ExportService {
     final path = '${directory.path}/${document.title}.json';
     final file = File(path);
     await file.writeAsString(jsonString);
+    return path;
+  }
+
+  Future<String> exportToPdf(NoteDocument document) async {
+    final pdf = pw.Document();
+
+    for (final page in document.pages) {
+      pdf.addPage(
+        pw.Page(
+          pageFormat: PdfPageFormat.a4,
+          build: (pw.Context context) {
+            return pw.CustomPaint(
+              painter: (canvas, size) {
+                for (final stroke in page.drawingData) {
+                  final path = pw.Path()
+                    ..moveTo(stroke.coordinates.first.dx,
+                        size.height - stroke.coordinates.first.dy);
+                  for (final point in stroke.coordinates.skip(1)) {
+                    path.lineTo(point.dx, size.height - point.dy);
+                  }
+                  canvas.drawPath(
+                    path,
+                    pw.Paint()
+                      ..color = PdfColor.fromInt(stroke.color)
+                      ..strokeWidth = stroke.width
+                      ..style = pw.PaintingStyle.stroke,
+                  );
+                }
+              },
+            );
+          },
+        ),
+      );
+    }
+
+    final directory = await getApplicationDocumentsDirectory();
+    final path = '${directory.path}/${document.title}.pdf';
+    final file = File(path);
+    await file.writeAsBytes(await pdf.save());
     return path;
   }
 }
