64e64c9bd8891dd82582796a67ca3d7515fb6d28
990aa <099ahadabdul@gmail.com>
2025-09-19 22:15:52 +0530

    state management

--- lib/features/notes/screens/note_editor_screen.dart
+++ lib/features/notes/screens/note_editor_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/blocs/document_bloc.dart';
+import 'package:kivixa/features/notes/blocs/document_event.dart';
+import 'package:kivixa/features/notes/blocs/document_state.dart';
+import 'package:kivixa/features/notes/blocs/drawing_bloc.dart';
+import 'package:kivixa/features/notes/blocs/drawing_event.dart';
+import 'package:kivixa/features/notes/blocs/drawing_state.dart';
 import 'package:kivixa/features/notes/widgets/notes_drawing_canvas.dart';
 
-class NoteEditorScreen extends StatelessWidget {
+class NoteEditorScreen extends StatefulWidget {
   final String? documentId;
 
   const NoteEditorScreen({super.key, this.documentId});
+
+  @override
+  State<NoteEditorScreen> createState() => _NoteEditorScreenState();
+}
+
+class _NoteEditorScreenState extends State<NoteEditorScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if (widget.documentId != null) {
+      context.read<DocumentBloc>().add(DocumentLoaded(widget.documentId!));
+    }
+    context.read<DrawingBloc>().add(DrawingStarted());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +33,35 @@ class NoteEditorScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Note Editor'),
       ),
-      body: const NotesDrawingCanvas(),
+      body: BlocBuilder<DocumentBloc, DocumentState>(
+        builder: (context, documentState) {
+          if (documentState is DocumentLoadInProgress) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (documentState is DocumentLoadSuccess) {
+            return BlocBuilder<DrawingBloc, DrawingState>(
+              builder: (context, drawingState) {
+                if (drawingState is DrawingState) {
+                  drawingState.notifier.addListener(() {
+                    final updatedDocument = documentState.document.copyWith(
+                      strokes: drawingState.notifier.currentSketch.strokes,
+                    );
+                    context.read<DocumentBloc>().add(DocumentContentChanged(updatedDocument));
+                  });
+                  return NotesDrawingCanvas(
+                    notifier: drawingState.notifier,
+                  );
+                } else {
+                  return const Center(child: CircularProgressIndicator());
+                }
+              },
+            );
+          } else if (documentState is DocumentLoadFailure) {
+            return Center(child: Text(documentState.error));
+          } else {
+            return const Center(child: Text('No document loaded.'));
+          }
+        },
+      ),
     );
   }
 }
