7506ee690d6b97c300f12e911280d23ee2880251
990aa <099ahadabdul@gmail.com>
2025-09-19 22:14:42 +0530

    state management

--- lib/features/notes/screens/notes_home_screen.dart
+++ lib/features/notes/screens/notes_home_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/blocs/notes_bloc.dart';
+import 'package:kivixa/features/notes/blocs/notes_event.dart';
+import 'package:kivixa/features/notes/blocs/notes_state.dart';
 
-class NotesHomeScreen extends StatelessWidget {
+class NotesHomeScreen extends StatefulWidget {
   const NotesHomeScreen({super.key});
+
+  @override
+  State<NotesHomeScreen> createState() => _NotesHomeScreenState();
+}
+
+class _NotesHomeScreenState extends State<NotesHomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<NotesBloc>().add(NotesLoaded());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,15 +24,28 @@ class NotesHomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Notes'),
       ),
-      body: ListView.builder(
-        itemCount: 10, // Placeholder for document list
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text('Note ${index + 1}'),
-            onTap: () {
-              Navigator.pushNamed(context, '/notes/editor', arguments: 'doc-id-${index + 1}');
-            },
-          );
+      body: BlocBuilder<NotesBloc, NotesState>(
+        builder: (context, state) {
+          if (state is NotesLoadInProgress) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (state is NotesLoadSuccess) {
+            return ListView.builder(
+              itemCount: state.notes.length,
+              itemBuilder: (context, index) {
+                final note = state.notes[index];
+                return ListTile(
+                  title: Text(note.title),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/notes/editor', arguments: note.id);
+                  },
+                );
+              },
+            );
+          } else if (state is NotesLoadFailure) {
+            return Center(child: Text(state.error));
+          } else {
+            return const Center(child: Text('No notes found.'));
+          }
         },
       ),
       floatingActionButton: FloatingActionButton(
