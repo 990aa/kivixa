cdfd347c1e9b82965d0eaa265628656337cf53d0
990aa <099ahadabdul@gmail.com>
2025-09-21 17:10:23 +0530

    colour scheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:kivixa/features/notes/screens/folder_management_screen.dart';
+import 'package:kivixa/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Kivixa',
-      theme: ThemeData.dark(),
-      home: const FolderManagementScreen(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          title: 'Kivixa',
+          theme: buildLightTheme(lightDynamic),
+          darkTheme: buildDarkTheme(darkDynamic),
+          home: const FolderManagementScreen(),
+        );
+      },
     );
   }
 }
