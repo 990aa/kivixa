31540d08241e9865f08cecf2817436ccaa27f0d5
990aa <099ahadabdul@gmail.com>
2025-09-21 12:32:20 +0530

    hero animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kivixa/features/notes/screens/folder_management_screen.dart';
+import 'package:kivixa/features/notes/screens/notes_home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Kivixa',
       theme: ThemeData.dark(),
-      home: const FolderManagementScreen(),
+      home: const NotesHomeScreen(),
     );
   }
 }
