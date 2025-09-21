f6b59868dafe5bd89a16727d989b2945018f4ea9
990aa <099ahadabdul@gmail.com>
2025-09-21 12:47:30 +0530

    modern search and filter

---
+++ lib/features/notes/widgets/filter_chips.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+
+class FilterChips extends StatelessWidget {
+  const FilterChips({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          NeumorphicButton(
+            onPressed: () {
+              // TODO: Implement date range picker
+            },
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.concave,
+              boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+              depth: 8,
+              lightSource: LightSource.topLeft,
+              color: Colors.grey[850],
+            ),
+            child: const Text('Date Range', style: TextStyle(color: Colors.white)),
+          ),
+          NeumorphicButton(
+            onPressed: () {
+              // TODO: Implement tag filter
+            },
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.concave,
+              boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+              depth: 8,
+              lightSource: LightSource.topLeft,
+              color: Colors.grey[850],
+            ),
+            child: const Text('Tags', style: TextStyle(color: Colors.white)),
+          ),
+          NeumorphicButton(
+            onPressed: () {
+              // TODO: Implement folder filter
+            },
+            style: NeumorphicStyle(
+              shape: NeumorphicShape.concave,
+              boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(12)),
+              depth: 8,
+              lightSource: LightSource.topLeft,
+              color: Colors.grey[850],
+            ),
+            child: const Text('Folder', style: TextStyle(color: Colors.white)),
+          ),
+        ],
+      ),
+    );
+  }
+}
