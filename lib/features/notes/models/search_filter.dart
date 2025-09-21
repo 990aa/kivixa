fec6af51111dd896c04126ffe56ec42e596e6ce5
990aa <099ahadabdul@gmail.com>
2025-09-21 12:45:15 +0530

    modern search and filter

---
+++ lib/features/notes/models/search_filter.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SearchFilter {
+  final DateTimeRange? dateRange;
+  final List<String> tags;
+  final String? folderId;
+  final SortOption sortOption;
+
+  SearchFilter({
+    this.dateRange,
+    this.tags = const [],
+    this.folderId,
+    this.sortOption = SortOption.name,
+  });
+
+  SearchFilter copyWith({
+    DateTimeRange? dateRange,
+    List<String>? tags,
+    String? folderId,
+    SortOption? sortOption,
+  }) {
+    return SearchFilter(
+      dateRange: dateRange ?? this.dateRange,
+      tags: tags ?? this.tags,
+      folderId: folderId ?? this.folderId,
+      sortOption: sortOption ?? this.sortOption,
+    );
+  }
+}
+
+enum SortOption {
+  name,
+  date,
+  size,
+  type,
+}
