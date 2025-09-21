dcbd29c50964e804e8fb6f4dd653b6df94381560
990aa <099ahadabdul@gmail.com>
2025-09-21 12:50:51 +0530

    context menu and action system

---
+++ lib/features/notes/widgets/folder_grid_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:kivixa/features/notes/widgets/context_menu.dart';
+
+class FolderGridView extends StatelessWidget {
+  const FolderGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+      ),
+      itemCount: 10, // Example count
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onLongPressStart: (details) {
+            showContextMenu(context, details.globalPosition);
+          },
+          child: Card(
+            color: Colors.grey[900],
+            child: Center(
+              child: Text(
+                'Folder ${index + 1}',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
