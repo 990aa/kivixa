4f102fc30587b16764e81431d7ba5d7d536ae629
990aa <099ahadabdul@gmail.com>
2025-09-22 21:33:34 +0530

    paper implementation

--- lib/features/notes/models/note_page.dart
+++ lib/features/notes/models/note_page.dart
@@ -1,23 +1,21 @@
 import 'dart:typed_data';
-
 import 'package:json_annotation/json_annotation.dart';
+import 'package:kivixa/features/notes/models/paper_settings.dart';
 import 'drawing_stroke.dart';
 
 part 'note_page.g.dart';
 
-@JsonSerializable(converters: [Uint8ListConverter()])
+@JsonSerializable(converters: [Uint8ListConverter(), PaperSettingsConverter()])
 class NotePage {
   final int pageNumber;
-  final String paperType;
-  final List<DrawingStroke> drawingData;
-  final Map<String, dynamic> backgroundSettings;
+  final List<DrawingStroke> strokes;
+  final PaperSettings paperSettings;
   final Uint8List? backgroundImage;
 
   NotePage({
     required this.pageNumber,
-    required this.paperType,
-    required this.drawingData,
-    required this.backgroundSettings,
+    required this.strokes,
+    required this.paperSettings,
     this.backgroundImage,
   });
 
@@ -28,16 +26,14 @@ class NotePage {
 
   NotePage copyWith({
     int? pageNumber,
-    String? paperType,
-    List<DrawingStroke>? drawingData,
-    Map<String, dynamic>? backgroundSettings,
+    List<DrawingStroke>? strokes,
+    PaperSettings? paperSettings,
     Uint8List? backgroundImage,
   }) {
     return NotePage(
       pageNumber: pageNumber ?? this.pageNumber,
-      paperType: paperType ?? this.paperType,
-      drawingData: drawingData ?? this.drawingData,
-      backgroundSettings: backgroundSettings ?? this.backgroundSettings,
+      strokes: strokes ?? this.strokes,
+      paperSettings: paperSettings ?? this.paperSettings,
       backgroundImage: backgroundImage ?? this.backgroundImage,
     );
   }
@@ -54,5 +50,40 @@ class Uint8ListConverter implements JsonConverter<Uint8List?, List<int>?> {
   @override
   List<int>? toJson(Uint8List? object) {
     return object?.toList();
+  }
+}
+
+class PaperSettingsConverter
+    implements JsonConverter<PaperSettings, Map<String, dynamic>> {
+  const PaperSettingsConverter();
+
+  @override
+  PaperSettings fromJson(Map<String, dynamic> json) {
+    final paperType = PaperType.values.byName(json['paperType']);
+    final optionsJson = json['options'] as Map<String, dynamic>;
+    switch (paperType) {
+      case PaperType.plain:
+        return PaperSettings(paperType: paperType, paperSize: PaperSize.a4, options: PlainPaperOptions.fromJson(optionsJson));
+      case PaperType.ruled:
+         return PaperSettings(paperType: paperType, paperSize: PaperSize.a4, options: RuledPaperOptions.fromJson(optionsJson));
+      case PaperType.grid:
+         return PaperSettings(paperType: paperType, paperSize: PaperSize.a4, options: GridPaperOptions.fromJson(optionsJson));
+      case PaperType.dotGrid:
+         return PaperSettings(paperType: paperType, paperSize: PaperSize.a4, options: DotGridPaperOptions.fromJson(optionsJson));
+      case PaperType.graph:
+         return PaperSettings(paperType: paperType, paperSize: PaperSize.a4, options: GraphPaperOptions.fromJson(optionsJson));
+    }
+  }
+
+  @override
+  Map<String, dynamic> toJson(PaperSettings object) {
+    return {
+      'paperType': object.paperType.name,
+      'paperSize': {
+        'width': object.paperSize.width,
+        'height': object.paperSize.height,
+      },
+      'options': object.options.toJson(),
+    };
   }
 }
