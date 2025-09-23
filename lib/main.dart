752e6d3b01a52a0a148463977d82630ac0f0bce4
990aa <099ahadabdul@gmail.com>
2025-09-23 07:39:01 +0530

    implement TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:dynamic_color/dynamic_color.dart';
-import 'package:flutter/material.dart';
+import 'package-flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kivixa/features/notes/blocs/folders_bloc.dart';
 import 'package:kivixa/features/notes/screens/folder_management_screen.dart';
+import 'package:kivixa/features/notes/services/notes_database_service.dart';
 import 'package:kivixa/theme.dart';
 
 void main() {
@@ -12,15 +15,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DynamicColorBuilder(
-      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
-        return MaterialApp(
-          title: 'Kivixa',
-          theme: buildLightTheme(lightDynamic),
-          darkTheme: buildDarkTheme(darkDynamic),
-          home: const FolderManagementScreen(),
-        );
-      },
+    return BlocProvider(
+      create: (context) => FoldersBloc(NotesDatabaseService.instance),
+      child: DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+          return MaterialApp(
+            title: 'Kivixa',
+            theme: buildLightTheme(lightDynamic),
+            darkTheme: buildDarkTheme(darkDynamic),
+            home: const FolderManagementScreen(),
+          );
+        },
+      ),
     );
   }
 }
