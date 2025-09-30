5893239d8b272a294d1e23bf722eb692fcbf3744
990aa <099ahadabdul@gmail.com>
2025-09-30 07:55:56 +0530

    errors

--- lib/helpers/database.dart
+++ lib/helpers/database.dart
@@ -8,9 +8,7 @@ class DatabaseHelper {
   Connection? _connection;
 
   Future<Connection> get connection async {
-    if (_connection == null) {
-      _connection = await _connect();
-    }
+    _connection ??= await _connect();
     return _connection!;
   }
 
