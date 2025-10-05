77aa5b84faa4029ac6c937533671f787f07fd8c0
990aa <099ahadabdul@gmail.com>
2025-10-05 11:36:39 +0530

    new implementation

--- lib/helpers/database.dart
+++
@@ -1,3 +0,0 @@
-export 'database_stub.dart'
-    if (dart.library.io) 'database_io.dart'
-    if (dart.library.html) 'database_web.dart';
--- lib/helpers/database_stub.dart
+++
@@ -1,6 +0,0 @@
-import 'package:postgres/postgres.dart';
-
-class DatabaseHelper {
-  Future<Connection> get connection => throw UnimplementedError();
-  Future<void> close() => throw UnimplementedError();
-}
