49cde59799d5c2936b581d678894e262eb9adea5
990aa <099ahadabdul@gmail.com>
2025-10-13 19:24:57 +0530

    errors fix

--- lib/widgets/toolbar_widget.dart
+++ lib/widgets/toolbar_widget.dart
@@ -206,7 +206,9 @@ class _ToolbarWidgetState extends State<ToolbarWidget>
                                   ),
                                   boxShadow: [
                                     BoxShadow(
-                                      color: Colors.black.withValues(alpha: 0.2),
+                                      color: Colors.black.withValues(
+                                        alpha: 0.2,
+                                      ),
                                       blurRadius: 4,
                                       offset: const Offset(0, 2),
                                     ),
