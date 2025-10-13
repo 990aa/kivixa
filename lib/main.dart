615e5fb7b891b7e522b6416d546cf2c593d39a8a
990aa <099ahadabdul@gmail.com>
2025-10-13 18:48:45 +0530

    pdf annotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'models/annotation_layer.dart';
+import 'models/drawing_tool.dart';
+import 'widgets/annotation_canvas.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +10,274 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Kivixa PDF Annotator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const PDFAnnotatorDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+/// Demo page showing the PDF annotation system in action
+class PDFAnnotatorDemo extends StatefulWidget {
+  const PDFAnnotatorDemo({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<PDFAnnotatorDemo> createState() => _PDFAnnotatorDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PDFAnnotatorDemoState extends State<PDFAnnotatorDemo> {
+  // Annotation layer to store all strokes
+  late AnnotationLayer _annotationLayer;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  // Current drawing settings
+  DrawingTool _currentTool = DrawingTool.pen;
+  Color _currentColor = Colors.black;
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _annotationLayer = AnnotationLayer();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+        title: const Text('Kivixa PDF Annotator'),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        actions: [
+          // Undo button
+          IconButton(
+            icon: const Icon(Icons.undo),
+            tooltip: 'Undo',
+            onPressed: () {
+              setState(() {
+                _annotationLayer.undoLastStroke();
+              });
+            },
+          ),
+          // Redo button
+          IconButton(
+            icon: const Icon(Icons.redo),
+            tooltip: 'Redo',
+            onPressed: () {
+              setState(() {
+                _annotationLayer.redoLastUndo();
+              });
+            },
+          ),
+          // Clear page button
+          IconButton(
+            icon: const Icon(Icons.delete_sweep),
+            tooltip: 'Clear Page',
+            onPressed: () {
+              setState(() {
+                _annotationLayer.clearPage(_currentPage);
+              });
+            },
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      body: Column(
+        children: [
+          // Toolbar
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.grey[200],
+            child: Row(
+              children: [
+                // Tool selector
+                const Text('Tool: ', style: TextStyle(fontWeight: FontWeight.bold)),
+                const SizedBox(width: 8),
+                _buildToolButton(DrawingTool.pen, Icons.edit, 'Pen'),
+                _buildToolButton(DrawingTool.highlighter, Icons.highlight, 'Highlighter'),
+                _buildToolButton(DrawingTool.eraser, Icons.auto_fix_high, 'Eraser'),
+                const SizedBox(width: 16),
+                
+                // Color selector (disabled for eraser)
+                if (_currentTool != DrawingTool.eraser) ...[
+                  const Text('Color: ', style: TextStyle(fontWeight: FontWeight.bold)),
+                  const SizedBox(width: 8),
+                  _buildColorButton(Colors.black),
+                  _buildColorButton(Colors.red),
+                  _buildColorButton(Colors.blue),
+                  _buildColorButton(Colors.green),
+                  _buildColorButton(Colors.yellow),
+                  _buildColorButton(Colors.orange),
+                ],
+                
+                const Spacer(),
+                
+                // Annotation stats
+                Text(
+                  'Total annotations: ${_annotationLayer.totalAnnotationCount}',
+                  style: const TextStyle(fontSize: 12),
+                ),
+              ],
             ),
-          ],
+          ),
+          
+          // Canvas area
+          Expanded(
+            child: Container(
+              color: Colors.grey[300],
+              child: Center(
+                child: Container(
+                  width: 595, // A4 width at 72 DPI
+                  height: 842, // A4 height at 72 DPI
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 10,
+                        offset: const Offset(0, 5),
+                      ),
+                    ],
+                  ),
+                  child: AnnotationCanvas(
+                    annotationLayer: _annotationLayer,
+                    currentPage: _currentPage,
+                    currentTool: _currentTool,
+                    currentColor: _currentColor,
+                    canvasSize: const Size(595, 842),
+                    onAnnotationsChanged: () {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Export annotations
+          FloatingActionButton(
+            heroTag: 'export',
+            onPressed: _exportAnnotations,
+            tooltip: 'Export Annotations',
+            child: const Icon(Icons.save_alt),
+          ),
+          const SizedBox(height: 8),
+          // Import annotations
+          FloatingActionButton(
+            heroTag: 'import',
+            onPressed: _importAnnotations,
+            tooltip: 'Import Annotations',
+            child: const Icon(Icons.file_upload),
+          ),
+        ],
+      ),
+    );
+  }
+
+  /// Builds a tool selection button
+  Widget _buildToolButton(DrawingTool tool, IconData icon, String label) {
+    final isSelected = _currentTool == tool;
+    return Padding(
+      padding: const EdgeInsets.only(right: 8.0),
+      child: ElevatedButton.icon(
+        onPressed: () {
+          setState(() {
+            _currentTool = tool;
+          });
+        },
+        icon: Icon(icon, size: 18),
+        label: Text(label),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: isSelected ? Colors.blue : Colors.grey[300],
+          foregroundColor: isSelected ? Colors.white : Colors.black,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  /// Builds a color selection button
+  Widget _buildColorButton(Color color) {
+    final isSelected = _currentColor == color;
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _currentColor = color;
+        });
+      },
+      child: Container(
+        width: 32,
+        height: 32,
+        margin: const EdgeInsets.only(right: 8),
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: isSelected ? Colors.blue : Colors.grey,
+            width: isSelected ? 3 : 1,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Exports annotations to JSON (in production, would save to file)
+  void _exportAnnotations() {
+    final jsonString = _annotationLayer.exportToJson();
+    
+    // In a real app, you would use file_picker and path_provider to save this
+    // For demo purposes, we'll just print it
+    debugPrint('Exported annotations:');
+    debugPrint(jsonString);
+    
+    // Show snackbar
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Exported ${_annotationLayer.totalAnnotationCount} annotations'),
+        action: SnackBarAction(
+          label: 'View',
+          onPressed: () {
+            _showJsonDialog(jsonString);
+          },
+        ),
+      ),
+    );
+  }
+
+  /// Imports annotations from JSON (in production, would load from file)
+  void _importAnnotations() {
+    // In a real app, you would use file_picker to select a file
+    // For demo purposes, we'll show a dialog
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Import feature: Use file_picker to select annotation file'),
+      ),
+    );
+  }
+
+  /// Shows JSON in a dialog for demo purposes
+  void _showJsonDialog(String jsonString) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Exported JSON'),
+        content: SingleChildScrollView(
+          child: SelectableText(jsonString),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Close'),
+          ),
+        ],
+      ),
     );
   }
 }
