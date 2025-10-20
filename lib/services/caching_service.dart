f2ed25565e9315119e4116ccc02c5bc13490398d
990aa <099ahadabdul@gmail.com>
2025-10-20 08:26:12 +0530

    v2

---
+++ lib/services/caching_service.dart
@@ -0,0 +1,17 @@
+import '../models/annotation_data.dart';
+
+class CachingService {
+  final Map<int, List<AnnotationData>> _cache = {};
+
+  List<AnnotationData>? getAnnotations(int pageNumber) {
+    return _cache[pageNumber];
+  }
+
+  void cacheAnnotations(int pageNumber, List<AnnotationData> annotations) {
+    _cache[pageNumber] = annotations;
+  }
+
+  void clearCache() {
+    _cache.clear();
+  }
+}
