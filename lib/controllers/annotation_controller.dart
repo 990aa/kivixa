5ebe4e91b3f91aed3174581ab90a8ec0ff23729c
990aa <099ahadabdul@gmail.com>
2025-10-20 08:20:55 +0530

    v2

---
+++ lib/controllers/annotation_controller.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/annotation_data.dart';
+import '../models/drawing_tool.dart';
+
+class AnnotationController extends ChangeNotifier {
+  final List<AnnotationData> _annotations = [];
+  DrawingTool _currentTool = DrawingTool.pen;
+  Color _currentColor = Colors.black;
+
+  List<AnnotationData> get annotations => _annotations;
+  DrawingTool get currentTool => _currentTool;
+  Color get currentColor => _currentColor;
+
+  void setCurrentTool(DrawingTool tool) {
+    _currentTool = tool;
+    notifyListeners();
+  }
+
+  void setCurrentColor(Color color) {
+    _currentColor = color;
+    notifyListeners();
+  }
+
+  void addAnnotation(AnnotationData annotation) {
+    _annotations.add(annotation);
+    notifyListeners();
+  }
+
+  void addPointsToCurrentAnnotation(List<Offset> points, int pageNumber) {
+    if (_annotations.isEmpty || _annotations.last.pageNumber != pageNumber) {
+      final newAnnotation = AnnotationData(
+        points: points,
+        color: _currentColor,
+        strokeWidth: _currentTool == DrawingTool.highlighter ? 12.0 : 4.0,
+        tool: _currentTool,
+        pageNumber: pageNumber,
+        timestamp: DateTime.now().millisecondsSinceEpoch,
+      );
+      addAnnotation(newAnnotation);
+    } else {
+      _annotations.last.points.addAll(points);
+    }
+    notifyListeners();
+  }
+
+  void clearAnnotationsForPage(int pageNumber) {
+    _annotations.removeWhere((a) => a.pageNumber == pageNumber);
+    notifyListeners();
+  }
+
+  void clearAllAnnotations() {
+    _annotations.clear();
+    notifyListeners();
+  }
+}
