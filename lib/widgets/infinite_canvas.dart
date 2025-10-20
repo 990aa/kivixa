83c6776e4c3f45ae77655ad728a85b6426eba521
990aa <099ahadabdul@gmail.com>
2025-10-20 08:45:56 +0530

    inf canvas to support elements

--- lib/widgets/infinite_canvas.dart
+++ lib/widgets/infinite_canvas.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:perfect_freehand/perfect_freehand.dart';
 import '../models/stroke.dart';
+import '../models/canvas_element.dart';
 import '../painters/infinite_canvas_painter.dart';
 
 /// Infinite canvas widget with pan and zoom capabilities
 class InfiniteCanvas extends StatefulWidget {
   final List<Stroke> initialStrokes;
+  final List<CanvasElement> initialElements;
   final Color currentColor;
   final double currentStrokeWidth;
   final bool isHighlighter;
   final Function(List<Stroke>)? onStrokesChanged;
+  final Function(List<CanvasElement>)? onElementsChanged;
 
   const InfiniteCanvas({
     super.key,
     this.initialStrokes = const [],
+    this.initialElements = const [],
     this.currentColor = Colors.black,
     this.currentStrokeWidth = 4.0,
     this.isHighlighter = false,
     this.onStrokesChanged,
+    this.onElementsChanged,
   });
 
   @override
@@ -27,6 +32,7 @@ class InfiniteCanvas extends StatefulWidget {
 class _InfiniteCanvasState extends State<InfiniteCanvas> {
   final TransformationController _controller = TransformationController();
   List<Stroke> strokes = [];
+  List<CanvasElement> elements = [];
   Offset canvasOffset = Offset.zero;
   bool isDrawing = false;
   List<Offset> currentPoints = [];
@@ -35,7 +41,35 @@ class _InfiniteCanvasState extends State<InfiniteCanvas> {
   void initState() {
     super.initState();
     strokes = List.from(widget.initialStrokes);
+    elements = List.from(widget.initialElements);
   }
+
+  // Public methods to add elements
+  void addElement(CanvasElement element) {
+    setState(() {
+      elements.add(element);
+      widget.onElementsChanged?.call(elements);
+    });
+  }
+
+  void updateElement(CanvasElement updatedElement) {
+    setState(() {
+      final index = elements.indexWhere((e) => e.id == updatedElement.id);
+      if (index != -1) {
+        elements[index] = updatedElement;
+        widget.onElementsChanged?.call(elements);
+      }
+    });
+  }
+
+  void removeElement(String elementId) {
+    setState(() {
+      elements.removeWhere((e) => e.id == elementId);
+      widget.onElementsChanged?.call(elements);
+    });
+  }
+
+  List<CanvasElement> getElements() => elements;
 
   @override
   void dispose() {
