31b4048e4e786e2ac0852a806eb5baa780e55018
990aa <099ahadabdul@gmail.com>
2025-11-13 21:29:58 +0530

    errors fix

--- lib/pages/project_manager/project_manager_page.dart
+++ lib/pages/project_manager/project_manager_page.dart
@@ -605,7 +605,7 @@ class _ProjectDetailsPageState extends State<ProjectDetailsPage>
               style: Theme.of(context).textTheme.titleSmall,
             ),
             const SizedBox(height: 8),
-            ...project.pendingChanges.map((change) => _buildChangeItem(change)),
+            ..._project.pendingChanges.map((change) => _buildChangeItem(change)),
           ],
         ],
       ),
