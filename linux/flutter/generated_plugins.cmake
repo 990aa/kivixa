c180f302c3ee8e177f981424a4dc6dd97ea7d4a7
990aa <099ahadabdul@gmail.com>
2025-11-25 20:31:06 +0530

    build and analyse fix

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -6,7 +6,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   desktop_webview_window
   dynamic_color
   flutter_secure_storage_linux
-  gtk
   irondash_engine_context
   open_file_linux
   printing
@@ -15,7 +14,6 @@ list(APPEND FLUTTER_PLUGIN_LIST
   url_launcher_linux
   window_manager
   window_to_front
-  yaru_window_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
