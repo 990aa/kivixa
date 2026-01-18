8be5487afabda2978db36817a6c9c73032b594c0
990aa <099ahadabdul@gmail.com>
2026-01-18 08:03:14 +0530

    bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -104,3 +104,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Enhanced settings page with improved layout
 
 ---
+## [0.1.5] - 2026-01-18
+
+### Changed
+- Version bump to 0.1.5
+
+---
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 1004;
+const buildNumber = 1005;
 
 /// The current app version as a string.
-const buildName = '0.1.4';
+const buildName = '0.1.5';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
