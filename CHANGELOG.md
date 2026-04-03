b3ce1cf99ec730c45ba3616273e537c3d92543ec
990aa <099ahadabdul@gmail.com>
2026-04-03 12:07:33 +0530

    fix: bump version to 0.3.11 and update related files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -441,3 +441,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Folder rename colour fixes
 
 ---
+## [0.3.11] - 2026-04-03
+
+### Changed
+- Version bump to 0.3.11
+
+---
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 3010;
+const buildNumber = 3011;
 
 /// The current app version as a string.
-const buildName = '0.3.10';
+const buildName = '0.3.11';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
