7696241732e8bc13f7716974db4347a5efb53d3e
990aa <099ahadabdul@gmail.com>
2026-04-09 21:29:26 +0530

    feat: bump version to 0.8.1 and update related references across the project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -553,3 +553,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
   - unknown tool rejection in MCP parser.
 
 ---
+## [0.8.1] - 2026-04-09
+
+### Changed
+- Version bump to 0.8.1
+
+---
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 8000;
+const buildNumber = 8001;
 
 /// The current app version as a string.
-const buildName = '0.8.0';
+const buildName = '0.8.1';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
