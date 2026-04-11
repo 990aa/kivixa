41eae43c4569d7f80f3251eb9093f626767c954a
990aa <099ahadabdul@gmail.com>
2026-04-11 10:35:33 +0530

    feat: bump version to 0.8.5 and update related files

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 8004;
+const buildNumber = 8005;
 
 /// The current app version as a string.
-const buildName = '0.8.4';
+const buildName = '0.8.5';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
