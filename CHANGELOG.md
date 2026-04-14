2f0cc80276585e476e935645301186ae8e210018
990aa <099ahadabdul@gmail.com>
2026-04-14 06:50:35 +0530

    chore: Bump version to 0.8.8 and update related files

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -634,3 +634,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 
 ---
+## [0.8.8] - 2026-04-14
+
+### Changed
+- Version bump to 0.8.8
+
+---
--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 8007;
+const buildNumber = 8008;
 
 /// The current app version as a string.
-const buildName = '0.8.7';
+const buildName = '0.8.8';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
