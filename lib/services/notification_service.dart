2034e276f657fc08a6ba21bd396aa6f19ea398bd
990aa <099ahadabdul@gmail.com>
2026-04-21 19:08:37 +0530

    refactor: simplify variable declarations and improve code consistency across notification services

--- lib/services/notification_service.dart
+++ lib/services/notification_service.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:math';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
