12d9b634099516b9c943d7edf568ea0ad93d492d
990aa <099ahadabdul@gmail.com>
2026-04-22 07:57:54 +0530

    chore: bump version to 0.8.15 and update related files

--- lib/data/version.dart
+++ lib/data/version.dart
@@ -4,10 +4,10 @@
 import 'package:kivixa/data/kivixa_version.dart';
 
 /// The current app version as an ordinal number.
-const buildNumber = 8014;
+const buildNumber = 8015;
 
 /// The current app version as a string.
-const buildName = '0.8.14';
+const buildName = '0.8.15';
 
 /// The year in which the current version was released.
 const buildYear = 2026;
